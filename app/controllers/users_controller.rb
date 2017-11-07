d1f229fe7e9dabc1c9d62adca5b39d324a2d7f68
Bui Huy Hoang <hoangbui237@gmail.com>
2017-11-07 10:35:22 +0700

    User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: %i(new create)
+
   def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:warning] = "cannot_find_user"
+    redirect_to root_path
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get  "/help", to: "static_pages#help"
   get  "/about", to: "static_pages#about"
   get  "/signup", to: "users#new"
-
-  get  "/signup",  to: "users#new"
+  resources :users
 end
